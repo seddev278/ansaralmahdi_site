f23db008db793b1626b508e154c442fde5ce7293
seyed reza <a.programer128@gmail.com>
2025-02-26 10:48:10 +0330

    heyat_project

--- heyat_site/heyat_site/settings.py
+++ heyat_site/heyat_site/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    
 ]
 
 MIDDLEWARE = [
@@ -99,7 +100,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
